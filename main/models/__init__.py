3e60f6bb090d62f9f51d0182d670efaada5348de
Morgan Heimbeck <morgan.heimbeck@bryantchristie.com>
2016-03-03 10:43:56 -0800

    moved the model files around

---
+++ main/models/__init__.py
@@ -0,0 +1,30 @@
+'''
+Load in the models for the bulk of the application
+'''
+from main.models.client import *
+from main.models.clientCode import *
+from main.models.authentication import *
+from main.models.company import *
+
+from main.models.carrier import *
+from main.models.carrierCode import *
+
+from main.models.address import *
+from main.models.city import *
+from main.models.country import *
+
+from main.models.shipment import *
+from main.models.security import *
+from main.models.eventGroup import *
+from main.models.event import *
+from main.models.contact import *
+from main.models.container import *
+from main.models.containerDetail import *
+from main.models.detail import *
+from main.models.stockKeepingUnit import *
+from main.models.seal import *
+
+from main.models.subscription import *
+from main.models.clientSubscription import *
+from main.models.currency import *
+from main.models.product import *
