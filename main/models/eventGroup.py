989a18077f64d5478179e7c41de04d8fa5962ea2
Morgan Heimbeck <morganryerheimbeck@gmail.com>
2016-03-05 23:21:31 -0800

    add mode to eventgroup

--- main/models/eventGroup.py
+++ main/models/eventGroup.py
@@ -4,15 +4,20 @@ from django.db import models
 from .main import Client,Tracking
 from .shipment import Shipment
 from .carrier import Carrier
-#
-#
-#
+'''
+grouping of shipment events that represent a shipment movement
+
+Usually a vessel, aircraft, truck, or rail
+'''
 
 class EventGroup(Tracking):
     Shipment = models.ForeignKey('Shipment')
     Client = models.ForeignKey('Client')
 
     Name = models.CharField(max_length=50) # Voyage/Flight Number?
+    # We do not support the idea of feeder/mother, All legs of a shipment are defined differently and we will not label them
+    MODES = [('A','Air'),('O','Ocean'),('R','Rail'),('T','Truck')]
+    Mode = models.CharField(max_length=1, choices=MODES, default='O')
     Sequence = models.IntegerField()
     BillNumber = models.CharField(max_length=12)
     Carrier = models.ForeignKey('Carrier')
@@ -22,5 +27,7 @@ class EventGroup(Tracking):
     #VesselName = models.CharField(max_length=50) #OPTIONAL
     #VesselOperator = models.ForeignKey(Carrier)
     #VoyageIdentifier = models.CharField(max_length=50) # Voyage/Flight Number?
+
+
 
     REQUIRED_FIELDS = ['Client','Shipment','Name','Sequence']
